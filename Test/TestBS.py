1849f9fcbbc15856cb336eca42f2555f93c5f152
AlexandrTretyakow1 <sahka2002@inbox.ru>
2023-05-15 23:04:18 +0300

    Clear version

--- Test/TestBS.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup as BS
-
-r = requests.get("https://store.softline.ru/parts/videocards/?page=1")
-print(r)
-html = BS(r.content, "html.parser")
-for el in html.select("h5"):
-    print(el)
